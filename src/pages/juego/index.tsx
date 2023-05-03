668208cbe1ee0fa10d32b77cf880f50be4bd4993
Valentino Conti <valeen.conti@hotmail.com>
2023-05-03 01:40:39 -0300

    Authenticate user when connecting to socket

--- src/pages/juego/index.tsx
+++ src/pages/juego/index.tsx
@@ -2,11 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import { Manrope } from "next/font/google";
 import styles from "../index.module.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ComeBackArrow } from "@/components/ComeBackArrow";
 import { Socket, io } from "socket.io-client";
 import { SERVER_URL } from "@/constants";
 import { useRouter } from "next/router";
+import { infoToast } from "@/utils/toasts";
+import FullscreenLoader from "@/components/FullscreenLoader";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -15,26 +17,51 @@ export default function Registro() {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   const [loggerUser, setLoggedUser] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isDisconnected = useRef(true);
 
   useEffect(() => {
+    console.log("hola?");
     const currentUser = localStorage.getItem("loggedUser") ?? "";
     if (!currentUser) {
       router.replace("/");
       return;
     }
-    setLoggedUser(currentUser);
 
     const newSocket = io(SERVER_URL, { withCredentials: true });
-    setSocket(newSocket);
+
+    setTimeout(() => {
+      if (isDisconnected.current) {
+        localStorage.setItem("loggedUser", "");
+        infoToast("No pudimos conectarnos, inicia sesión nuevamente", 4000, "socketNo");
+        router.push("/");
+      }
+    }, 6500);
+
+    newSocket.on("connect", () => {
+      console.log("connected!");
+      isDisconnected.current = false;
+
+      setIsLoading(false);
+      setSocket(newSocket);
+    });
+
+    newSocket.on("username", username => {
+      localStorage.setItem("loggedUser", username);
+      setLoggedUser(username);
+    });
 
     newSocket.on("otherMessage", message => {
       console.log("message id", message.id);
-      console.log("socket id", newSocket.id);
-      console.log("user", message.user);
 
       if (message.id !== newSocket.id) {
-        setOtherInput(message.user + ": " + message.value);
+        setOtherInput(message.value);
       }
+    });
+
+    newSocket.on("disconnect", _reason => {
+      router.replace("/");
     });
 
     return () => {
@@ -51,6 +78,8 @@ export default function Registro() {
         <title>CryptoTruco | Juego</title>
       </Head>
 
+      {isLoading && <FullscreenLoader />}
+
       <div className={styles.container}>
         <ComeBackArrow />
         <div className={styles.logo}>
@@ -61,7 +90,7 @@ export default function Registro() {
 
         <div className={styles.separator} />
 
-        <div>JUEGO TEST</div>
+        <div>{loggerUser}</div>
 
         <input
           value={userInput}
@@ -72,7 +101,6 @@ export default function Registro() {
               socket.emit("message", {
                 id: socket.id,
                 value: ev.target.value,
-                user: loggerUser,
               });
             }
           }}
